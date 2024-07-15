1167e259b4e2a6b5a302b5d7e98be50c34970f6a
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2024-07-15 23:47:58 +0530

    first commit

---
+++ Redux-React/src/Components/CakeContainers.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CakeContainers() {
+  return (
+    <div>
+      <p>Number of Cakes</p>
+      <button>Buy cake</button>
+    </div>
+  )
+}
+
+export default CakeContainers
