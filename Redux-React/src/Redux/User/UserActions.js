708f56bcdb5aa0ec70d6bdc9756083c546cafe40
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2024-07-19 22:41:44 +0530

    Redux-thunk

---
+++ Redux-React/src/Redux/User/UserActions.js
@@ -0,0 +1,35 @@
+import { FETCH_USER_FAILURE, FETCH_USER_REQUEST, FETCH_USER_SUCCESS } from "./UserTypes"
+import axios from 'axios'
+
+const fetchUserSuccess = users => {
+    return{
+        type : FETCH_USER_SUCCESS,
+        payload : users
+    }
+}
+
+export const fetchUserRequest = () =>{
+    return{
+        type : FETCH_USER_REQUEST
+    }
+}
+
+const fetchUserFailure = error =>{
+    return {
+        type : FETCH_USER_FAILURE,
+        payload : error
+    }
+}
+
+export const fetchUsers = () => {
+    dispatch(fetchUserRequest)
+    return (dispatch) => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(response =>{const users = response.data
+            dispatch(fetchUserSuccess(users))
+        })
+        .catch(error =>{const errorMsg = error.message
+            dispatch(fetchUserFailure(errorMsg))
+        })
+    }
+}
--- Redux-React/src/Redux/rootReduser.js
+++ Redux-React/src/Redux/rootReduser.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import  cakeReduser from './Cake/cakeReduser'
 import iceReducer from './IceCream/iceReducer';
+import UserReducer from './User/UserReducer';
 
 const rootReduser = combineReducers({
   cake: cakeReduser,
-  ice : iceReducer
+  ice : iceReducer,
+  user : UserReducer
 });
 
 export default rootReduser;
